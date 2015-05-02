be3ddd1f9e94aab02c8cbcb18c18c5390ffe622f
Alex Berry <berry3@indiana.edu>
2015-05-02 15:59:48 -0400

    adding failing test

--- src/test/kotlin/org/libraryweasel/minkfx/MinkTest.kt
+++ src/test/kotlin/org/libraryweasel/minkfx/MinkTest.kt
@@ -3,6 +3,7 @@ package org.libraryweasel.minkfx
 import javafx.scene.Node
 import javafx.scene.layout.GridPane
 import javafx.scene.text.Text
+import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import org.junit.Test as test
 
@@ -10,5 +11,18 @@ class MinkTest {
     test fun emptyTest() {
         val node: Node = mink {}
         assertNotNull(node)
+    }
+
+    test fun addSingleTextNodeTest() {
+        val hello = Text("Hello minkfx")
+        val node = mink { +hello }
+
+        if (node is GridPane) {
+            val children = node.getChildren()
+            assertEquals(1, children.size())
+            assertEquals(hello, children[0])
+        } else {
+            throw Exception("node isn't instance of GridPane")
+        }
     }
 }
